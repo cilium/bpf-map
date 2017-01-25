782193bf42368e15294bd0ba8c8fa5db65e356ec
Tobias Klauser <tklauser@distanz.ch>
2017-01-25 14:26:37 +0100

    Godeps: update cilium
    Update the cilium godep to get the new bpf map types.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- vendor/github.com/cilium/cilium/common/const.go
+++ vendor/github.com/cilium/cilium/common/const.go
@@ -21,26 +21,12 @@ var (
 )
 
 const (
-	// Cilium constants
-
 	// CiliumPath is the path where cilium operational files are running.
 	CiliumPath    = "/var/run/cilium"
 	DefaultLibDir = "/usr/lib/cilium"
 	CiliumUIPath  = DefaultLibDir + "/ui"
 	// CiliumSock is the cilium socket for the communication between the daemon and cilium client.
 	CiliumSock = CiliumPath + "/cilium.sock"
-	// BPFMap is the file that contains the BPF Map for the host.
-	BPFMapRoot    = "/sys/fs/bpf"
-	BPFCiliumMaps = BPFMapRoot + "/tc/globals"
-	BPFMap        = BPFCiliumMaps + "/cilium_lxc"
-	// Basename prefix of endpoint specific policy map
-	PolicyMapName = "cilium_policy_"
-	// Path prefix to endpoint specific policy map
-	PolicyMapPath = BPFCiliumMaps + "/" + PolicyMapName
-	Ct6MapName    = "cilium_ct6_"
-	BPFMapCT6     = BPFCiliumMaps + "/" + Ct6MapName
-	Ct4MapName    = "cilium_ct4_"
-	BPFMapCT4     = BPFCiliumMaps + "/" + Ct4MapName
 	// RFC3339Milli is the RFC3339 with milliseconds for the default timestamp format
 	// log files.
 	RFC3339Milli = "2006-01-02T15:04:05.000Z07:00"
@@ -58,8 +44,6 @@ const (
 	LabelIDKeyPath = OperationalPath + "/Labels/IDs"
 	// MaxSetOfLabels is maximum number of set of labels that can be stored in consul.
 	MaxSetOfLabels = uint32(0xFFFF)
-	// FirstFreeLabelID is the first ID for which the labels should be assigned.
-	FirstFreeLabelID = uint32(256)
 	// LastFreeServiceIDKeyPath is the path where the Last free UUID is stored in consul.
 	LastFreeServiceIDKeyPath = OperationalPath + "/Services/LastUUID"
 	// ServiceKeyPath is the base path where services are stored in consul.
@@ -82,12 +66,17 @@ const (
 	// K8sAnnotationName is the annotation name used for the cilium policy name in the
 	// kubernetes network policy.
 	K8sAnnotationName = "io.cilium.name"
+	// K8sLabelPrefix is the default prefix used when parsing labels that don't have
+	// the GlobalLabelPrefix in kubernetes.
+	K8sLabelPrefix = "io.cilium.k8s."
 	// K8sPodNamespaceLabel is the label used in kubernetes containers to specify
 	// which namespace they belong to.
 	K8sPodNamespaceLabel = "io.kubernetes.pod.namespace"
 	// K8sAnnotationParentName is the annotation name used for the cilium policy
 	// parent name in the kubernetes network policy.
 	K8sAnnotationParentName = "io.cilium.parent"
+	// K8s environment variable label
+	K8sEnvNodeNameSpec = "K8S_NODE_NAME"
 	// Label source for reserved types
 	ReservedLabelSource = "reserved"
 	// Label used to represent the reserved source
